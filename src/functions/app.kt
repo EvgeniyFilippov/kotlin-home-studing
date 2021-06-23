0de7d73ad4aa976c35b4cd80a272fbed136318e4
Yauhen Filipau <6479556@gmail.com>
2021-06-23 20:46:28 +0300

    add default method

--- src/functions/app.kt
+++ src/functions/app.kt
@@ -4,7 +4,7 @@ fun main() {
     val a: Int = 10
     val b: Int = 15
 
-    fun sum(a: Int, b: Int) : Int {
+    fun sum(a: Int, b: Int): Int {
         return a + b
     }
     println(sum(a, b))
@@ -13,4 +13,13 @@ fun main() {
         println("Hello")
     }
     sayHello()
+
+    var mySum = sum(2, 2)
+    println(mySum)
+
+    //функция со значениями по-умолчанию
+    fun mySumWithDefault(a: Int, b: Int = 5, c: Int = 5): Int{
+        return a + b + c
+    }
+    println(mySumWithDefault(1))
 }
